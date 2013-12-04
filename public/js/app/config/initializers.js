23f2e0f48377d35085bc7f5e788498e450074cc0
ellmo <ellmunzai@gmail.com>
2013-12-04 17:55:16 +0100

    finally got jade runtime into require-js

--- public/js/app/config/initializers.js
+++ public/js/app/config/initializers.js
@@ -1,3 +1,24 @@
-requirejs.config({
+// window.__JST = {};
+
+require.config({
   baseUrl: 'js/',
+  paths: {
+    jade: 'lib/jaderuntime'
+  },
+  shim: {
+    jade: {
+      exports: 'jade'
+    }
+  }
 });
+
+// function JST(template_path){
+//   if( __JST[template_path] ){ return __JST[template_path] };
+//   try {
+//     requirejs([template_path], function(){
+//       __JST[template_path] = anonymous
+//     })
+//   } catch(error) {
+//     debugger;
+//   }
+// };
