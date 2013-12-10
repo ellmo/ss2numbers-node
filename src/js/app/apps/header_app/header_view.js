5e8e4d0a75d1354e05d40ba7ca9cf75e1e1e3df9
ellmo <ellmunzai@gmail.com>
2013-12-10 23:09:56 +0100

    removed all old src/ js files and unused jst

--- src/js/app/apps/header_app/header_view.js
+++
@@ -1,6 +0,0 @@
-this.SS2N.module("HeaderApp.Header", function(Header, App, Backbone, Marionette, $, _) {
-
-  Header.View = Backbone.Marionette.ItemView.extend({
-    template: "header"
-  });
-});
--- src/js/app/config/marionette/renderer.js
+++
@@ -1,7 +0,0 @@
-Backbone.Marionette.Renderer.render = function(template, data){
-  path = JST[template];
-  if (!path) {
-    throw "Template " + template + " not found!";
-  }
-  return path(data);
-}
